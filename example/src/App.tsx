b96a9ec2d4c5cf91b66eb3eea20c5e33186774ae
sunrise1002 <tunglm.mar@gmail.com>
2024-07-04 11:16:24 +0700

    feat: setup example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,9 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  useColorScheme,
-} from 'react-native';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-import List from './List'
+import { SafeAreaView, useColorScheme } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import List from './List';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -18,14 +14,12 @@ function App(): React.JSX.Element {
   };
 
   return (
-    <GestureHandlerRootView style={{flex: 1}}>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaView style={backgroundStyle}>
         <List />
       </SafeAreaView>
     </GestureHandlerRootView>
   );
 }
-
-const styles = StyleSheet.create({});
 
 export default App;
